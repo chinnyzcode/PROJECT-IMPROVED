5508fdf933f2ed4dd12d61c24ad17f9dcd45b2f9
chinnyzcode <124817430+chinnyzcode@users.noreply.github.com>
2023-02-21 09:33:28 +0100

    Injected the HTML multiple framework for the forecast

--- script.js
+++ script.js
@@ -22,6 +22,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class= row>`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temp-max"> 18
+                  </span>
+                  <span class="weather-forecast-temp-min"> 12
+                  </span>
+                </div>
+              </div>
+        `;
+  });
+  forecastHTML = forecastHTML + `</div> `;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -89,3 +118,5 @@ fahreinheitLink.addEventListener("click", displayFahreinheitTemperature);
 
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
+
+displayForecast();
